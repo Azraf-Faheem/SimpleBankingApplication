56fb8b135bdfb93c130fc5e3e36d73b696333934
Azraf-Faheem <azraf.faheem.42152@gmail.com>
2024-01-31 02:28:51 +0600

    User and admin Login page added with css update

---
+++ view/adminLoginForm.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles.css">
+    <title>Admin Login</title>
+</head>
+<body>
+<nav class="navbar">
+        <h1>WELCOME TO Admin Login </h1>
+        <button class="home"><a href="../view/landingPage.php">Home</a></button>
+    </nav>
+    <br>
+    <br>
+    <br>
+    <br>
+    <form>
+   <center> Username: <input type="text" class="username"> <br> </center>
+   <center> Password: <input type="password" class="password"> <br> </center>
+   <center><input type="submit" value="Login"></center>
+</form>
+    <footer class="footer">
+        <p>&copy; 2024 All Rights Reserved by AZRAF FAHEEM</p>
+    </footer>
+</body>
+</html>
--- view/landingPage.php
+++ view/landingPage.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="../css/styles.css">
     <title>Homepage</title>
 </head>
 <body>
@@ -14,10 +14,10 @@
     &nbsp
     &nbsp
     &nbsp
-    <button class="center"><a href='#'>Admin Login</a></button>
+    <button class="center"><a href="../view/adminLoginForm.php">Admin Login</a></button>
     &nbsp
     &nbsp
-    <button class="center"><a href='#'>User Login</a></button>
+    <button class="center"><a href="../view/userLoginForm.php">User Login</a></button>
     <footer class="footer">
         <p>&copy; 2024 All Rights Reserved by AZRAF FAHEEM</p>
     </footer>
