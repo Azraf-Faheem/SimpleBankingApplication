d6671a641dfc2cd5bcf18fc5bd3031586b110302
Azraf-Faheem <azraf.faheem.42152@gmail.com>
2024-01-30 20:17:50 +0600

    Landing page added

---
+++ view/landingPage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Homepage</title>
+</head>
+<body>
+    <nav class="navbar">
+        <h1>WELCOME TO BANKING APPLICATION</h1>
+    </nav>
+    &nbsp
+    &nbsp
+    &nbsp
+    &nbsp
+    <button class="center"><a href='#'>Admin Login</a></button>
+    &nbsp
+    &nbsp
+    <button class="center"><a href='#'>User Login</a></button>
+    <footer class="footer">
+        <p>&copy; 2024 All Rights Reserved by AZRAF FAHEEM</p>
+    </footer>
+</body>
+</html>
